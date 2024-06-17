68868b82566a49122f1411aad4f986333ca0783d
pidanmeng <1092333914@qq.com>
2024-06-17 14:50:31 +0800

    feat: 🎸 update request baseurl

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -12,7 +12,7 @@ interface AxiosInterceptorManager<V> {
 }
 
 const request = axios.create({
-  baseURL: 'https://koiism.cn:8000',
+  baseURL: 'https://koiism.cn',
 });
 
 request.interceptors.request.use(async (config) => {
